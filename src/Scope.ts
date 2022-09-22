e2a81abc5e8336f95eaf80026a194dbf4370c769
Tyler Schrock <tschrock123@gmail.com>
2022-09-22 12:20:24 -0400

    Update scopes

--- src/Scope.ts
+++ src/Scope.ts
@@ -1,31 +1,140 @@
 export enum Scope {
 
-    /** View analytics data for your extensions. */
+    /** View analytics data for the Twitch Extensions owned by the authenticated account. */
     ANALYTICS_READ_EXTENSIONS = "analytics:read:extensions",
 
-    /** View analytics data for your games. */
+    /** View analytics data for the games owned by the authenticated account. */
     ANALYTICS_READ_GAMES = "analytics:read:games",
 
-    /** View Bits information for your channel. */
+    /** View Bits information for a channel. */
     BITS_READ = "bits:read",
 
-    /** Get a list of all subscribers to your channel and check if a user is subscribed to your channel */
+    /** Run commercials on a channel. */
+    CHANNEL_EDIT_COMMERCIAL = "channel:edit:commercial",
+
+    /** Manage a channel’s broadcast configuration, including updating channel configuration and managing stream markers and stream tags. */
+    CHANNEL_MANAGE_BROADCAST = "channel:manage:broadcast",
+
+    /** Read charity campaign details and user donations on your channel. */
+    CHANNEL_READ_CHARITY = "channel:read:charity",
+
+    /** Manage a channel’s Extension configuration, including activating Extensions. */
+    CHANNEL_MANAGE_EXTENSIONS = "channel:manage:extensions",
+
+    /** Add or remove the moderator role from users in your channel. */
+    CHANNEL_MANAGE_MODERATORS = "channel:manage:moderators",
+
+    /** Manage a channel’s polls. */
+    CHANNEL_MANAGE_POLLS = "channel:manage:polls",
+
+    /** Manage of channel’s Channel Points Predictions */
+    CHANNEL_MANAGE_PREDICTIONS = "channel:manage:predictions",
+
+    /** Manage a channel raiding another channel. */
+    CHANNEL_MANAGE_RAIDS = "channel:manage:raids",
+
+    /** Manage Channel Points custom rewards and their redemptions on a channel. */
+    CHANNEL_MANAGE_REDEMPTIONS = "channel:manage:redemptions",
+
+    /** Manage a channel’s stream schedule. */
+    CHANNEL_MANAGE_SCHEDULE = "channel:manage:schedule",
+
+    /** Manage a channel’s videos, including deleting videos. */
+    CHANNEL_MANAGE_VIDEOS = "channel:manage:videos",
+
+    /** View a list of users with the editor role for a channel. */
+    CHANNEL_READ_EDITORS = "channel:read:editors",
+
+    /** View Creator Goals for a channel. */
+    CHANNEL_READ_GOALS = "channel:read:goals",
+
+    /** View Hype Train information for a channel. */
+    CHANNEL_READ_HYPE_TRAIN = "channel:read:hype_train",
+
+    /** View a channel’s polls. */
+    CHANNEL_READ_POLLS = "channel:read:polls",
+
+    /** View a channel’s Channel Points Predictions. */
+    CHANNEL_READ_PREDICTIONS = "channel:read:predictions",
+
+    /** View Channel Points custom rewards and their redemptions on a channel. */
+    CHANNEL_READ_REDEMPTIONS = "channel:read:redemptions",
+
+    /** View an authorized user’s stream key. */
+    CHANNEL_READ_STREAM_KEY = "channel:read:stream_key",
+
+    /** View a list of all subscribers to a channel and check if a user is subscribed to a channel. */
     CHANNEL_READ_SUBSCRIPTIONS = "channel:read:subscriptions",
 
-    /** Manage a clip object. */
+    /** Read the list of VIPs in your channel. */
+    CHANNEL_READ_VIPS = "channel:read:vips",
+
+    /** Add or remove the VIP role from users in your channel. */
+    CHANNEL_MANAGE_VIPS = "channel:manage:vips",
+    /** Manage Clips for a channel. */
     CLIPS_EDIT = "clips:edit",
+    /** View a channel’s moderation data including Moderators, Bans, Timeouts, and Automod settings. */
+    MODERATION_READ = "moderation:read",
+
+    /** Send announcements in channels where you have the moderator role. */
+    MODERATOR_MANAGE_ANNOUNCEMENTS = "moderator:manage:announcements",
+
+    /** Manage messages held for review by AutoMod in channels where you are a moderator. */
+    MODERATOR_MANAGE_AUTOMOD = "moderator:manage:automod",
+
+    /** View a broadcaster’s AutoMod settings. */
+    MODERATOR_READ_AUTOMOD_SETTINGS = "moderator:read:automod_settings",
+
+    /** Manage a broadcaster’s AutoMod settings. */
+    MODERATOR_MANAGE_AUTOMOD_SETTINGS = "moderator:manage:automod_settings",
+
+    /** Ban and unban users. */
+    MODERATOR_MANAGE_BANNED_USERS = "moderator:manage:banned_users",
+
+    /** View a broadcaster’s list of blocked terms. */
+    MODERATOR_READ_BLOCKED_TERMS = "moderator:read:blocked_terms",
+
+    /** Manage a broadcaster’s list of blocked terms. */
+    MODERATOR_MANAGE_BLOCKED_TERMS = "moderator:manage:blocked_terms",
+
+    /** Delete chat messages in channels where you have the moderator role */
+    MODERATOR_MANAGE_CHAT_MESSAGES = "moderator:manage:chat_messages",
+
+    /** View a broadcaster’s chat room settings. */
+    MODERATOR_READ_CHAT_SETTINGS = "moderator:read:chat_settings",
+
+    /** Manage a broadcaster’s chat room settings. */
+    MODERATOR_MANAGE_CHAT_SETTINGS = "moderator:manage:chat_settings",
 
     /** Manage a user object. */
     USER_EDIT = "user:edit",
 
-    /** Edit your channel’s broadcast configuration, including extension configuration. (This scope implies user:read:broadcast capability.) */
-    USER_EDIT_BROADCAST = "user:edit:broadcast",
+    /** Deprecated. Was previously used for “Create User Follows” and “Delete User Follows.” See Deprecation of Create and Delete Follows API Endpoints. */
+    USER_EDIT_FOLLOWS = "user:edit:follows",
 
-    /** View your broadcasting configuration, including extension configurations. */
+    /** Manage the block list of a user. */
+    USER_MANAGE_BLOCKED_USERS = "user:manage:blocked_users",
+
+    /** View the block list of a user. */
+    USER_READ_BLOCKED_USERS = "user:read:blocked_users",
+
+    /** View a user’s broadcasting configuration, including Extension configurations. */
     USER_READ_BROADCAST = "user:read:broadcast",
 
-    /** Read authorized user’s email address. */
+    /** Update the color used for the user’s name in chat.Update User Chat Color */
+    USER_MANAGE_CHAT_COLOR = "user:manage:chat_color",
+
+    /** View a user’s email address. */
     USER_READ_EMAIL = "user:read:email",
+
+    /** View the list of channels a user follows. */
+    USER_READ_FOLLOWS = "user:read:follows",
+
+    /** View if an authorized user is subscribed to specific channels. */
+    USER_READ_SUBSCRIPTIONS = "user:read:subscriptions",
+
+    /** Read whispers that you send and receive, and send whispers on your behalf. */
+    USER_MANAGE_WHISPERS = "user:manage:whispers",
 
     /** Read whether a user is subscribed to your channel. */
     V5_CHANNEL_CHECK_SUBSCRIPTION = "channel_check_subscription",
